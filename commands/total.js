3567ebf4c85bef6c2adacf6cf06e82bce80580cb
CodeEnder <alricost@gmail.com>
2020-07-16 18:01:48 -0400

    Added total command

---
+++ commands/total.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = {
+	name: 'total',
+	description: 'The total amount of `gCoin` in the system',
+	usage: '',
+	args: false,
+	execute(message) {
+		const rawdata = fs.readFileSync('./ledger.json');
+		const ledgerArray = JSON.parse(rawdata);
+
+		let total = 0;
+
+		ledgerArray.forEach((item) => {
+			total += item.gCoin;
+		});
+
+		message.channel.send('The total amount of `gCoin` in the system is: ' + total);
+	},
+};
